6376e9cefbb5d6cff5329735e30f4688086e632a
Carlos Niño <cf.ninor1@uniandes.edu.co>
2025-04-26 05:10:06 -0500

    Se agrega servicë del vehiculo

---
+++ src/app/vehicle/service/vehicle.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Vehicle } from '../vehicle';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehicleService {
+  private apiUrl = environment.baseUrl + '202212_MISW4104_Grupo1.json';
+
+  constructor(private http: HttpClient) { }
+
+  getVehicles() {
+    return this.http.get<Vehicle[]>(this.apiUrl);
+  }
+
+}
