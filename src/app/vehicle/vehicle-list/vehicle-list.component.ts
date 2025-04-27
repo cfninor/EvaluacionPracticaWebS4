d1accfb005c4b25c0f3601ee8af1ad0f64089a5d
Carlos Niño <cf.ninor1@uniandes.edu.co>
2025-04-26 22:34:28 -0500

    Se agrega contador de vehiculos por marca

--- src/app/vehicle/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle/vehicle-list/vehicle-list.component.ts
@@ -11,11 +11,20 @@ import { VehicleService } from '../service/vehicle.service';
 export class VehicleListComponent implements OnInit {
 
   vehicles : Array<Vehicle> = [];
+  vehiclesCount: Map<string, number> = new Map();
   constructor(private vehicleService: VehicleService) { }
 
   getVehicles() {
     this.vehicleService.getVehicles().subscribe(vehicles => {
       this.vehicles = vehicles;
+      this.getNumberOfVehicles();
+    });
+  }
+
+  getNumberOfVehicles(): void {
+    this.vehicles.forEach(vehicle => {
+      const marca = vehicle.marca;
+      this.vehiclesCount.set(marca, (this.vehiclesCount.get(marca) || 0) + 1);
     });
   }
 
